8bfb5b23285b4803ddfe013d0062bf5c77b25cee
lebrancconvas <lebrancconvas@gmail.com>
2025-06-15 03:18:41 +0700

    ✨ feat: update get register and add fetch (8-bits & 16-bits) methods.

--- lesson_01/utils/cpu.ts
+++ lesson_01/utils/cpu.ts
@@ -11,19 +11,20 @@ export class CPU {
   constructor(size: number) {
     this.memory = Memory.create(size);
     this.registers = Memory.create(REGISTERS.length * 2);
-    
+
     this.registerMap = new Map<Register, number>();
     REGISTERS.map((register: Register, index: number) => {
       this.registerMap.set(register, index * 2);
     })
   }
 
+  // Set & Get Register Value.
   getRegister(registerName: Register) {
     if(!registerName) {
       throw new Error(`[ERROR] ${ErrorLog.REG_NOT_FOUND}\nRegister: ${registerName}`);
     }
 
-    this.registers.getUint16(this.registerMap.get(registerName) as number) ;
+    return this.registers.getUint16(this.registerMap.get(registerName) as number) ;
   };
 
   setRegister(registerName: Register, value: number) {
@@ -32,6 +33,21 @@ export class CPU {
     }
 
     this.registers.setUint16(this.registerMap.get(registerName) as number, value);
+  }
+
+  // CPU Process
+  fetch() {
+    const nextInstructionAddress = this.getRegister("ip");
+    const instruction = this.memory.getUint8(nextInstructionAddress);
+    this.setRegister("ip", nextInstructionAddress + 1);
+    return instruction;
+  }
+
+  fetch16() {
+    const nextInstructionAddress = this.getRegister("ip");
+    const instruction = this.memory.getUint16(nextInstructionAddress);
+    this.setRegister("ip", nextInstructionAddress + 2);
+    return instruction;
   }
 
 };
