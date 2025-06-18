7d39cc2daeaff57d849990afcf9c76a2b2c55646
lebrancconvas <lebrancconvas@gmail.com>
2025-06-19 01:48:39 +0700

    ✨ feat(lesson_03_04): add new registers for implementation of stack section of the memory.

--- lesson_03_04/utils/register.ts
+++ lesson_03_04/utils/register.ts
@@ -1,7 +1,8 @@
 export const REGISTERS = [
-  "ip", "acc",
-  "r1", "r2", "r3", "r4",
-  "r5", "r6", "r7", "r8"
+  "ip", "acc", // ip => Instruction Pointer, acc => Accumulator
+  "r1", "r2", "r3", "r4", // r1 - r8 => General-Purpose Registers
+  "r5", "r6", "r7", "r8",
+  "sp", "fp" // sp => Stack Pointer, fp => Frame Pointer
 ] as const;
 
 export type Register = typeof REGISTERS[number];
