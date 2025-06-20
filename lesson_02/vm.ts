36f89e29bf486d1be6ea9ad6560e22f989aa7a28
lebrancconvas <lebrancconvas@gmail.com>
2025-06-21 00:08:23 +0700

    ✨ feat(lesson_02): imply the readline and add move reg to mem assembler.

--- lesson_02/vm.ts
+++ lesson_02/vm.ts
@@ -4,6 +4,8 @@ import {
   Instruction
 } from "~/utils";
 
+import { readLine } from "./helpers/readline";
+
 import { CPU_SETTING } from "~/constants";
 
 function vm() {
@@ -13,6 +15,7 @@ function vm() {
 
   const R1 = cpu.registerMap.get("r1") as number;
   const R2 = cpu.registerMap.get("r2") as number;
+  const ACC = cpu.registerMap.get("acc") as number;
 
   const writeableRAM = new Uint8Array(memory.buffer);
   const assemblers: (number | Instruction)[] = [
@@ -28,24 +31,25 @@ function vm() {
 
     Instruction.ADD_REG_REG,
     R1,
-    R2
+    R2,
+
+    Instruction.MOV_REG_MEM,
+    ACC,
+    0x01,
+    0x00 // Memory Address: 0x0100
   ];
 
   assemblers.map((assemble: number, index: number) => {
     writeableRAM[index] = assemble;
   })
 
-  cpu.viewMemory(0x0000);
-  cpu.debug();
 
-  cpu.step();
-  cpu.debug();
-
-  cpu.step();
-  cpu.debug();
-
-  cpu.step();
-  cpu.debug();
+  readLine.on("line", () => {
+    cpu.viewMemory(0x0100);
+    cpu.viewMemory(cpu.getRegister("ip"));
+    cpu.debug();
+    cpu.step();
+  });
 };
 
 export default vm;
